4f6d7d4e62f01e01ee2a318516e6f10889d063bd
Ramill <ramill_ibragimov@icloud.com>
2023-08-11 09:25:33 +0300

    Initial Commit

---
+++ DiscoveringTipKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiscoveringTipKit
+//
+//  Created by Ramill Ibragimov on 11.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiscoveringTipKit/DiscoveringTipKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoveringTipKitApp.swift
+//  DiscoveringTipKit
+//
+//  Created by Ramill Ibragimov on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscoveringTipKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
